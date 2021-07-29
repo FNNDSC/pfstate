2d39497ca99878067e88e647d46796a15dfde64a
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2021-07-29 15:48:19 -0400

    Bump to 3.0.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 setup(
       name             =   'pfstate',
-      version          =   '2.2.12',
+      version          =   '3.0.0',
       description      =   'class-defintion stateful module',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
